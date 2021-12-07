75a525bc4a9acd165abcb5c41da0128891af5fa4
AlulaLeak <alula.leakemariam94@gmail.com>
2021-12-07 00:31:25 +0000

    added notes (mostly for my own understanding

--- express-server.js
+++ express-server.js
@@ -1,31 +1,34 @@
-const express = require("express");
+const express = require("express"); // brought in express
 const app = express();
 const PORT = 8000; // default port 8080
 
-const urlDatabase = {
+const urlDatabase = { // Defined our database
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.set('view engine', 'ejs');
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+app.set('view engine', 'ejs'); // made a view engine and set it to ejs
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Example app listening on port ${PORT}!`); // made our server/app listen on port 8000 for a command
+});
+
+    // Routes //
+
+app.get("/", (req, res) => { // made the server/app able to get route '/' , for our client/browser
+  res.render("index"); // sent a render response
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.render("index");
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase }; // made object of template variables
+  res.render("urls_index", templateVars); // made the templateVars object contents available to use in EJS file urls_index
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+app.get("/urls/:shortURL", (req, res) => { // made a route with a parameter
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars); // made the templateVars object contents available to use in EJS file urls_shows
 });
