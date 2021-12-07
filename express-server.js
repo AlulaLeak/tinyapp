b01630d53daf3a5d8e839703b472efc80a0c0dd6
AlulaLeak <alula.leakemariam94@gmail.com>
2021-12-07 20:21:56 +0000

    removed urls routes

--- express-server.js
+++ express-server.js
@@ -3,10 +3,6 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8000; // default port 8080
 
-    // body parser middleware to convert Buffer to Human-Readable String
-
-app.use(bodyParser.urlencoded({extended: true}));
-
     // Temporary Database 
 
 const urlDatabase = { // Defined our database
@@ -14,38 +10,63 @@ const urlDatabase = { // Defined our database
   "9sm5xK": "http://www.google.com"
 };
 
+    // body parser middleware to convert Buffer to Human-Readable String
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs'); // made a view engine and set it to ejs
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`); // made our server/app listen on port 8000 for a command
+  console.log(urlDatabase)
 });
 
-    // Single Path Route //
-
-app.get("/", (req, res) => { // made the server/app able to get route '/' , for our client/browser
-  res.render("index"); // sent a render response
-  console.log('hey')
+    // Routes //
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
-    // paths with parameters
-
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+})
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
-  res.redirect(longURL);
+  const longURL = req.body.longURL
+  res.redirect(urlDatabase[req.params.shortURL]);
 });
 
-  // ^ urls path in ./routes/urls.js ^ \\
-
-  const urlRouter = require('./routes/urls')
-  app.use('/urls', urlRouter)
+app.post("/urls", (req, res) => {
+  const tinyURL = generateRandomString(6)
+  console.log(req.body.longURL);  // Log the POST request body to the console
+  res.redirect(301, `/urls/${tinyURL}`);         // Respond with 'Ok' (we will replace this)
+  urlDatabase[tinyURL] = req.body.longURL
+  console.log(urlDatabase)
+  
+});
 
     // Generate Random String Function
 
+function generateRandomString(length) {
+const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
 
 
 
-module.exports = urlDatabase
